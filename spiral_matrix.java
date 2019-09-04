e9e0b87ee00f1c6445b90e9e3f259e551548ea5e
Pooja Ramaswamy <poojaramaswamy94@gmail.com>
2019-09-03 18:36:55 -0700

    Completed Array 1 Problems

---
+++ spiral_matrix.java
@@ -0,0 +1,67 @@
+//O(n) time complexity
+//O(1) space complexity
+//Tried in leetcode and accepted on submission
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(matrix.length == 0 || matrix[0].length == 0) return res;
+        
+        int top = 0;
+        int bottom = matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
+        
+        while(true){
+            //iterates from 00 to 02 in case of example 1
+            for(int i = left; i <= right; i++)
+            {
+                res.add(matrix[top][i]);
+            }
+            //Increment top by 1
+            top++;
+            //Check for boundry conditions
+            if(left > right || top > bottom) 
+            {
+                break;
+            }
+            //Iterate from 12 to 22 in ex 1 
+            for(int i = top; i <= bottom; i++) 
+            {
+                res.add(matrix[i][right]);
+            }
+            //decrement right by 1
+            right--;
+            //Check for boundry condition again
+            if(left > right || top > bottom) 
+            {
+                break;
+            }
+            
+            //Iterate from 21 to 20
+            for(int i = right; i >= left; i--) 
+            {
+                res.add(matrix[bottom][i]);
+            }
+            //decrement bottom by 1
+            bottom--;
+            if(left > right || top > bottom) 
+            {
+                break;
+            }
+            //Iterate for 10
+            for(int i = bottom; i >= top; i--) 
+            {
+                res.add(matrix[i][left]);
+            }
+            //Increment left by 1
+            left++;
+            if(left > right || top > bottom) 
+            {
+                break;
+            }
+        }
+        
+        return res;
+    }
+    
+}
